05717f7a22c892b95bd856ef25e6b0aa5a459bf1
flowbogii <flow.bogii@gmail.com>
2024-11-29 16:59:21 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render stellt $PORT bereit
+    app.run(host="0.0.0.0", port=port)
+
+
 app = Flask(__name__)
 CORS(app)  # Aktiviert CORS für alle Routen
 
